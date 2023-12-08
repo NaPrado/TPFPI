c8f65d80368e452e53c8a430aed445e06a33880f
NaPrado <naprado@itba.edu.ar>
2023-12-08 13:20:11 -0300

    primera aproximacion de los main

--- bikeSharingNYC.c
+++ bikeSharingNYC.c
@@ -1,7 +1,19 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "stationADT.h"
+#include "readNYC.h"
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    time_t t=time(NULL);
+    stationADT new= newStation();
+    inicializerNYCFormat(argv,new);
+    query1(new);
+    query2(new);
+    query3(new);
+    freeStation(new);
+    freeAssets();
+    printf("%ld\n",time(NULL)-t);
     return 0;
 }
--- stationADT.h
+++ stationADT.h
@@ -1,6 +1,7 @@
 #ifndef __stationADT_h_
 #define __stationADT_h_
 #include <stdlib.h>
+#include <stdio.h>
 typedef struct stationCDT * stationADT; 
 typedef struct stationsIdNode *stationsIdBST;
 typedef struct station * pStation ;
@@ -19,6 +20,14 @@ void addRental(stationsIdBST idBST, struct tm * startDate,size_t startId,struct 
 //quiza no haga falta 
 stationADT deleteStation();
 
-void freeStation();
+void query1(stationADT stations);
+
+void query2(stationADT stations);
+
+void query3(stationADT stations);
+
+void freeStation(stationADT stations);
+
+void freeAssets();
 
 #endif
